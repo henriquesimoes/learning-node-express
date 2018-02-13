c8e84a66640a2149feb44ad83d9eb362b65f7052
Henrique Simões <henriquesimoespa@gmail.com>
2018-02-13 18:53:24 -0200

    Add insert and retrieve from database

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 'use strict';
 
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
+var staff = require('./router/staff');
+
+const MONGO_URL = "mongodb://localhost:27017/";
 
 const PORT = 8080;
 const HOST = "localhost";
 
-app.all('/', (req, res) => {
-    res.send('<p>It´s working just fine!</p>');
+MongoClient.connect(MONGO_URL, (err, db) => {
+    if(err) err;
+    var dbo = db.db("mydb");
+    staff.setDatabase(dbo);
+    console.log('Database is up...');
 });
+
+app.all('/', (req, res) => {
+    res.send('Everything okay!');
+});
+
+app.use('/staff', staff.router);
 
 app.listen(PORT, HOST, (err) => {
     if(err) throw err;
