053448c260b223e6e841fdd0d4c68435c12a02f6
Henrique Simões <henriquesimoespa@gmail.com>
2018-07-09 21:48:28 -0300

    Fix auth code to use new user controller functions

--- api/lib/auth.js
+++ api/lib/auth.js
@@ -4,13 +4,11 @@ const passport = require('passport');
 const BasicStrategy = require('passport-http').BasicStrategy;
 const userController = require('../controller/user');
 
-passport.use(new BasicStrategy((email, password, done) => {
-  userController.retrieveUsers({email: email}, (err, user) => {
-    if (err) return done(err);
-    if (user === undefined) return done(null, false);
-    if (user.password !== password) return done(null, false);
-    return done(null, user);
-  });
+passport.use(new BasicStrategy(async (email, password, done) => {
+  const user = await userController.retrieveUsers({email});
+  if (!user) return done(null, false);
+  if (user.password !== password) return done(null, false);
+  return done(null, user);
 }));
 
 module.exports = passport;
